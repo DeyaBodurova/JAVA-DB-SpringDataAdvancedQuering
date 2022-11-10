e3c0c45d0c0ff8936e1051659e077fe9fe5286bb
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-10 07:03:22 +0200

    wip

--- src/main/java/com/example/springdataadvancedquering/SpringDataAdvancedQueryingApplication.java
+++ src/main/java/com/example/springdataadvancedquering/SpringDataAdvancedQueryingApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDataAdvancedQueringApplication {
+public class SpringDataAdvancedQueryingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringDataAdvancedQueringApplication.class, args);
+        SpringApplication.run(SpringDataAdvancedQueryingApplication.class, args);
     }
 
 }
+
